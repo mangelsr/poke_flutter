82bf006405c14a7e3774af7c426b61d935f54d89
Miguel Sanchez <mangelsr25@gmail.com>
2021-09-11 23:26:25 -0500

    [UPD] SpeciesCard shows real reight and weight and make convertion

--- lib/widgets/species_card.dart
+++ lib/widgets/species_card.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'package:poke_flutter/models/pokemon.dart';
+import 'package:poke_flutter/providers/api_provider.dart';
 import 'package:poke_flutter/widgets/custom_card.dart';
 
 class SpeciesCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ApiProvider apiProvider = Provider.of<ApiProvider>(context);
+    final Pokemon pokemon = apiProvider.selectedPokemon;
+
+    final double weightKilograms = pokemon.weight / 10;
+    final double weightPounds = weightKilograms * 2.20462;
+    final String weightKilogramsRounded = weightKilograms.toStringAsFixed(2);
+    final String weightPoundsRounded = weightPounds.toStringAsFixed(2);
+
+    final double heightMeters = pokemon.height / 10;
+    final double heightFeets = heightMeters * 3.28084;
+    final int heightInches = ((heightFeets - heightFeets.floor()) * 12).round();
+    final int heightFeetsRounded = heightFeets.floor();
+    final String heightMetersRounded = heightMeters.toStringAsFixed(2);
+
     return CustomCard(
       child: Column(
         children: [
@@ -19,15 +36,17 @@ class SpeciesCard extends StatelessWidget {
                 flex: 1,
                 child: _SpeciesData(
                   title: 'Height',
-                  content: '2\'4\'\' (0.71 m)',
+                  content:
+                      '${heightFeetsRounded}\'${heightInches}\'\' (${heightMetersRounded} m)',
                 ),
               ),
               SizedBox(width: 10),
               Expanded(
                 flex: 1,
                 child: _SpeciesData(
-                  title: 'Height',
-                  content: '15.21 lbs (6.9 kg)',
+                  title: 'Weight',
+                  content:
+                      '${weightPoundsRounded} lbs (${weightKilogramsRounded} kg)',
                 ),
               ),
             ],

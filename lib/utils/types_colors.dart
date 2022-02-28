bc984be59cc0dae74a8d2f01f9916ce24070375f
Miguel Sanchez <mangelsr25@gmail.com>
2022-02-28 00:38:29 -0500

    [ADD] methods for ligthen and darken type colors

--- lib/utils/types_colors.dart
+++ lib/utils/types_colors.dart
@@ -22,3 +22,18 @@ Map<String, Color> typesColors = {
   'shadow': Colors.black,
   'unknown': Colors.black,
 };
+
+//  ranges from 0.0 to 1.0
+Color darken(Color color, [double amount = .1]) {
+  assert(amount >= 0 && amount <= 1);
+  final hsl = HSLColor.fromColor(color);
+  final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+  return hslDark.toColor();
+}
+
+Color lighten(Color color, [double amount = .1]) {
+  assert(amount >= 0 && amount <= 1);
+  final hsl = HSLColor.fromColor(color);
+  final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+  return hslLight.toColor();
+}
